e825dd8cc21ccbd355d92fd669a5e715ba0aae12
Aline-JCB <ilo9@live.fr>
2024-11-23 21:02:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "884c5a2f4583f875d9d021b222e1d752")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7449323563:AAFXpGPmqCWnlLBm120L3XN0q0likLyjqqg") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Yukti:Sh@cluster0.uy7bs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "cluster")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
